78fb5ae33f047db476b9dbcd459332dcdcf75348
0 <0>
2025-01-14 18:10:05 +0100

    udp communication

---
+++ Excercise 2/udp.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func main() {
+	addr := ":20009"
+	conn, err := net.ListenPacket("udp", addr)
+	if err != nil {
+		fmt.Println("Error starting")
+		fmt.Println(err)
+		return
+	}
+	q := []byte("Hello")
+	server_addr := &net.UDPAddr{
+		IP:   net.ParseIP("10.100.23.204"),
+		Port: 20009,
+	}
+	conn.WriteTo(q, server_addr)
+
+	p := make([]byte, 1024)
+	n, client_addr, client_err := conn.ReadFrom(p)
+	if client_err != nil {
+		fmt.Println("Client Error")
+		fmt.Println(client_err)
+		return
+	}
+	fmt.Println(string(p[0:n]))
+	fmt.Println(client_addr)
+
+}
