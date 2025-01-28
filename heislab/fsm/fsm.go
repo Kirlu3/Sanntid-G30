98ba4148f76e254cb5ada0ae5ba2d238c9b0af87
Tora Bøckman <toraboc@stud.ntnu.no>
2025-01-28 20:10:08 +0100

    fixing fsm.go

--- heislab/fsm/fsm.go
+++ heislab/fsm/fsm.go
@@ -4,7 +4,8 @@ import "fmt"
 import "../driver-go/elevio"
 import "time"
 
-var doorOpenDuration int := 3
+var doorOpenDuration = time.Second * 3
+
 var ob = make(chan bool)
 
 type ElevatorBehaviour int
@@ -15,21 +16,20 @@ const (
 )
 
 type Elevator struct {
-	floor int
-	direction elevio.Direction
-	requests var int
+	floor int 
+	direction elevio.MotorDirection 
+	requests [0]int
 	behaviour ElevatorBehaviour
 }
-
-Elevator elevator := 
-
+var elevator = Elevator{-1, elevio.MD_Stop, [0]int{}, EB_Idle}
 
 func timer(t_start chan bool, t_end chan bool) {
 	t_on := false
+	t := time.Now() 
 
 	for {
 		if <- t_start {
-			t := time.Now() 
+			t = time.Now() 
 			t_on = true
 		}
 		if time.Now() - t > doorOpenDuration && t_on {
@@ -41,53 +41,53 @@ func timer(t_start chan bool, t_end chan bool) {
 
 func onRequestButtonPress(buttonEvent elevio.ButtonEvent, t_start chan bool) {
 	fmt.Println("onRequestButtonPress")
-	switch Elevator.ElevatorBehaviour{
-	case ElevatorBehaviour.EB_DoorOpen:
-		if Elevator.floor == buttonEvent.Floor {
+	switch elevator.behaviour{
+	case EB_DoorOpen:
+		if elevator.floor == buttonEvent.Floor {
 			t_start <- true
 		} else {
 			//Add to queue?
 			//Don't know if we ever should get here
 		}
 		//Do nothing
-	case ElevatorBehaviour.EB_Moving:
-		Elevator.request = buttonEvent.Floor
+	case EB_Moving:
+		elevator.requests = buttonEvent.Floor
 		//There will be a bug here if we allow more than one item in the queue at a time
-	case ElevatorBehaviour.EB_Idle:
-		Elevator.request = buttonEvent.Floor
-		switch Elevator.floor {
-		case buttonEvent.Floor == Elevator.floor:
+	case EB_Idle:
+		elevator.requests = buttonEvent.Floor
+		switch elevator.floor {
+		case buttonEvent.Floor == elevator.floor:
 			//Open door
 			t_start <- true
-			Elevator.behaviour = ElevatorBehaviour.EB_DoorOpen
+			elevator.behaviour = EB_DoorOpen
 			elevio.SetDoorOpenLamp(true)
-		case buttonEvent.Floor > Elevator.floor:
+		case buttonEvent.Floor > elevator.floor:
 			//move up
-			Elevator.direction = elevio.MD_Up
-			Elevator.behaviour = ElevatorBehaviour.EB_Moving
-		case ButtonEvent.Floor < Elevator.floor:
+			elevator.direction = elevio.MD_Up
+			elevator.behaviour = EB_Moving
+		case ButtonEvent.Floor < elevator.floor:
 			//move down
-			Elevator.direction = elevio.MD_Down
-			Elevator.behaviour = ElevatorBehaviour.EB_Moving
+			elevator.direction = elevio.MD_Down
+			elevator.behaviour = EB_Moving
 	}
-	elevio.SetMotorDirection(Elevator.direction)
+	elevio.SetMotorDirection(elevator.direction)
 	return
 }}
 
 
 func onFloorArrival(floor int, t_start chan bool) {
 	fmt.Println("onFloorArrival")
-	Elevator.floor = floor
+	elevator.floor = floor
 	elevio.SetFloorIndicator(floor)
 
-	if Elevator.request == Elevator.floor {
-		Elevator.behaviour = ElevatorBehaviour.EB_DoorOpen
-		Elevator.direction = elevio.MD_Stop
-		elevio.SetMotorDirection(Elevator.direction)
+	if elevator.requests == elevator.floor {
+		elevator.behaviour = EB_DoorOpen
+		elevator.direction = elevio.MD_Stop
+		elevio.SetMotorDirection(elevator.direction)
 		elevio.SetDoorOpenLamp(true)
 		t_start <- true 
 		//Clears queue
-		Elevator.request = -1
+		elevator.requests = -1
 	}
 	//Send a completion message?
 	return
@@ -104,37 +104,34 @@ func onStopButtonPress() {
 	fmt.Println("You pressed the stop button :)")
 	return
 }
-
-
-func onTimerEnd() {
-	//Temporary, not implemented obstruction
+time.Time(time.Now().Year(), 01, 01, 0, 0, 0, 0, time.UTC)tion
 	for <-ob {
 		ob <- true
 	} 
 
 	ob <- false
 	//Checks where the next request is and sets associated direction and behaviour
-	switch Elevator.request {
-	case Elevator.request == -1:
-		Elevator.behaviour = ElevatorBehaviour.EB_Idle	
-	case Elevator.request > Elevator.floor:
-		Elevator.direction = elevio.MD_Up
-		Elevator.behaviour = ElevatorBehaviour.EB_Moving
-	case Elevator.request < Elevator.floor:
-		Elevator.direction = elevio.MD_Down
-		Elevator.behaviour = ElevatorBehaviour.EB_Moving
-	case Elevator.request == Elevator.floor:
-		Elevator.behaviour = ElevatorBehaviour.EB_DoorOpen
+	switch elevator.requests {
+	case -1:
+		elevator.behaviour = EB_Idle	
+	case elevator.requests > elevator.floor:
+		elevator.direction = elevio.MD_Up
+		elevator.behaviour = EB_Moving
+	case elevator.requests < elevator.floor:
+		elevator.direction = elevio.MD_Down
+		elevator.behaviour = EB_Moving
+	case elevator.requests == elevator.floor:
+		elevator.behaviour = EB_DoorOpen
 	}
 
 	//Sets the elevator to the new behaviour
 	switch ElevatorBehaviour {
-	case ElevatorBehaviour.EB_DoorOpen:
+	case EB_DoorOpen:
 		t_start <- true
-	case ElevatorBehaviour.EB_Moving:
+	case EB_Moving:
 		elevio.SetDoorOpenLamp(false)
-		elevio.SetMotorDirection(Elevator.direction)
-	case ElevatorBehaviour.EB_Idle:
+		elevio.SetMotorDirection(elevator.direction)
+	case EB_Idle:
 		elevio.SetDoorOpenLamp(false)		
 	return
 	}
