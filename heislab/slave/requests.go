6736a026525e1a7f00b6b20c38733ef4d720a243
Kirlu3 <ulrikgarmarkanker@gmail.com>
2025-01-30 09:51:43 +0100

    finished requests.go

--- heislab/slave/requests.go
+++ heislab/slave/requests.go
@@ -1,5 +1,7 @@
 package slave
 
+import "../driver-go/elevio"
+
 type DirectionBehaviorPair struct {
 	direction ElevatorDirection
 	behavior  ElevatorBehaviour
@@ -70,4 +72,63 @@ func requests_chooseDirection(elevator Elevator) DirectionBehaviorPair {
 		}
 	}
 	return DirectionBehaviorPair{D_Stop, EB_Idle}
+}
+
+func requests_shouldStop(elevator Elevator) bool {
+	switch elevator.direction {
+	case D_Down:
+		if elevator.requests[elevator.floor][elevio.BT_HallDown] {
+			return true
+		}
+		if elevator.requests[elevator.floor][elevio.BT_Cab] {
+			return true
+		}
+		if !requests_below(elevator) {
+			return true
+		}
+		return false
+	case D_Up:
+		if elevator.requests[elevator.floor][elevio.BT_HallUp] {
+			return true
+		}
+		if elevator.requests[elevator.floor][elevio.BT_Cab] {
+			return true
+		}
+		if !requests_above(elevator) {
+			return true
+		}
+		return false
+	case D_Stop:
+	}
+	return true
+}
+
+func requests_shouldClearImmediately(elevator Elevator, btn_floor int, btn_type elevio.ButtonType) bool {
+	//only case CV_InDirn from C:
+	return elevator.floor == btn_floor && ((elevator.direction == D_Up && btn_type == elevio.BT_HallUp) ||
+		(elevator.direction == D_Down && btn_type == elevio.BT_HallDown) ||
+		elevator.direction == D_Stop ||
+		btn_type == elevio.BT_Cab)
+}
+
+func requests_clearAtCurrentFloor(elevator Elevator) Elevator {
+	//only case CV_InDirn from C:
+	elevator.requests[elevator.floor][elevio.BT_Cab] = false //I doubt we can actually clear in this way
+	switch elevator.direction {
+	case D_Up:
+		if !requests_above(elevator) && !elevator.requests[elevator.floor][elevio.BT_HallDown] {
+			elevator.requests[elevator.floor][elevio.BT_HallDown] = false
+		}
+		elevator.requests[elevator.floor][elevio.BT_HallUp] = false
+	case D_Down:
+		if !requests_below(elevator) && !elevator.requests[elevator.floor][elevio.BT_HallUp] {
+			elevator.requests[elevator.floor][elevio.BT_HallUp] = false
+		}
+		elevator.requests[elevator.floor][elevio.BT_HallDown] = false
+	case D_Stop:
+	default:
+		elevator.requests[elevator.floor][elevio.BT_HallUp] = false
+		elevator.requests[elevator.floor][elevio.BT_HallDown] = false
+	}
+	return elevator
 }
