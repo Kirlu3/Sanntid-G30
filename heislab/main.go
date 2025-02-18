6d1a217e8055adf06a8283d2c26092cbba8b3e00
Kirlu3 <ulrikgarmarkanker@gmail.com>
2025-02-18 17:21:22 +0100

    also changed Slave to slave in main

--- heislab/main.go
+++ heislab/main.go
@@ -3,14 +3,14 @@ package main
 import (
 	"github.com/Kirlu3/Sanntid-G30/heislab/backup"
 	"github.com/Kirlu3/Sanntid-G30/heislab/driver-go/elevio"
-	Slave "github.com/Kirlu3/Sanntid-G30/heislab/slave"
+	"github.com/Kirlu3/Sanntid-G30/heislab/slave"
 )
 
 func main() {
 	id := "2"
 	N_FLOORS := 4
 	elevio.Init("localhost:15657", N_FLOORS)
-	go Slave.Slave()
+	go slave.Slave()
 	go backup.Backup(id)
 
 	select {}
